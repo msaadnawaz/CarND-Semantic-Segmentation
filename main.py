5475332d4dfcbb2b437c64708d80984e2d94bd03
Muhammad Saad Nawaz <saadnawaz27@yahoo.com>
2018-02-13 16:06:12 +0100

    epochs correction for print

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     for epoch in range(epochs):
         for image, label in get_batches_fn(batch_size):
             _, training_loss = sess.run([train_op, cross_entropy_loss] , feed_dict={input_image: image, correct_label: label, keep_prob: 0.8, learning_rate: 1e-5})    
-        print("Epoch: %d of %d ; Loss: %.4f" %( epoch+1, epoch, training_loss))
+        print("Epoch: %d of %d ; Loss: %.4f" %( epoch+1, epochs, training_loss))
 tests.test_train_nn(train_nn)
 
 
